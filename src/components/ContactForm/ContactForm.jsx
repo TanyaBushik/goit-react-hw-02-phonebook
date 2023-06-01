36561eb25e52915d7b58281b6d1b7e7b6ba202b3
TanyaBushik <tanya.bushik@gmail.com>
2023-06-01 12:42:22 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
   Form,
   NameInput,
@@ -65,6 +66,8 @@ class ContactForm extends Component {
   }
 }
 
-// ContactForm.propTypes = {};
+ContactForm.propTypes = {
+  onAdd: PropTypes.func.isRequired,
+};
 
 export default ContactForm;
